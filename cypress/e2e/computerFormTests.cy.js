c43714534554c7be7da47fbc98f121a59d940438
Lucas Olmos <lucasolmos@gmail.com>
2023-10-29 16:45:56 -0300

    Add intercept command to catch request and assert status.

--- cypress/e2e/computerFormTests.cy.js
+++ cypress/e2e/computerFormTests.cy.js
@@ -6,14 +6,21 @@ describe('computer form tests', () => {
   })
   
   it('creates a new computer by completing only mandatory fields', () => {
+    cy.intercept('POST', '/computers')
+      .as('addComputer')
     cy.get('#add').click()
     cy.url().should('include','/new')
     cy.get('#name').type("New Computer")
     cy.get('.btn.primary').click()
+    cy.wait('@addComputer')
+      .its('response.statusCode')
+      .should('eq', 303)
     cy.get('.alert-message.warning').should('have.text','Done !  Computer New Computer has been created')
   })
 
   it('creates a new computer by completing all fields', () => {
+    cy.intercept('POST', '/computers')
+      .as('addComputer')
     cy.get('#add').click()
     cy.url().should('include','/new')
     cy.get('#name').type("New Computer")
@@ -21,6 +28,9 @@ describe('computer form tests', () => {
     cy.get('#discontinued').type("2023-01-25")
     cy.get('#company').select('Sony')
     cy.get('.btn.primary').click()
+    cy.wait('@addComputer')
+      .its('response.statusCode')
+      .should('eq', 303)
     cy.get('.alert-message.warning').should('have.text','Done !  Computer New Computer has been created')
   })
 })
