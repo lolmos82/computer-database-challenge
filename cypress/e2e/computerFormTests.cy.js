66e7558fcbf410e20cceac7d9d5c6fb6ecef4a99
Lucas Olmos <lucasolmos@gmail.com>
2023-10-29 16:27:08 -0300

    update tests to check url path

--- cypress/e2e/computerFormTests.cy.js
+++ cypress/e2e/computerFormTests.cy.js
@@ -7,6 +7,7 @@ describe('computer form tests', () => {
   
   it('creates a new computer by completing only mandatory fields', () => {
     cy.get('#add').click()
+    cy.url().should('include','/new')
     cy.get('#name').type("New Computer")
     cy.get('.btn.primary').click()
     cy.get('.alert-message.warning').should('have.text','Done !  Computer New Computer has been created')
@@ -14,6 +15,7 @@ describe('computer form tests', () => {
 
   it('creates a new computer by completing all fields', () => {
     cy.get('#add').click()
+    cy.url().should('include','/new')
     cy.get('#name').type("New Computer")
     cy.get('#introduced').type("2022-01-25")
     cy.get('#discontinued').type("2023-01-25")
